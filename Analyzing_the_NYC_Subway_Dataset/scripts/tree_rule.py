bf250512f0014783ac6051ea0035a4ab7fa389b5
ShaneKao <ko8342010@yahoo.com.tw>
2016-02-28 00:35:48 +0800

    Added files via upload

---
+++ Analyzing_the_NYC_Subway_Dataset/scripts/tree_rule.py
@@ -0,0 +1,20 @@
+def get_rule(tree, feature_names):
+        left      = tree.tree_.children_left
+        right     = tree.tree_.children_right
+        threshold = tree.tree_.threshold
+        features  = [feature_names[i] for i in tree.tree_.feature]
+        value = tree.tree_.value
+
+        def recurse(left, right, threshold, features, node):
+                if (threshold[node] != -2):
+                        print "if ( " + features[node] + " < " + str(threshold[node]) + " ) {"
+                        if left[node] != -1:
+                                recurse (left, right, threshold, features,left[node])
+                        print "} else {"
+                        if right[node] != -1:
+                                recurse (left, right, threshold, features,right[node])
+                        print "}"
+                else:
+                        print "return " + str(value[node])
+
+        recurse(left, right, threshold, features, 0)
